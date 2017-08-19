25b1d1fef02a650b7a38a9f05b124ec320fa9306
richard <richard.heuser@gfk.com>
2017-08-19 18:16:11 +0200

    dimishing returns and moving window

--- server/routes.js
+++ server/routes.js
@@ -21,7 +21,7 @@ module.exports = function (router, db) {
 	});
 	
 	var thumbs = function (req, res, upDown) {
-		var sql = 'INSERT INTO points (hash_from, country_to, license_to, score) VALUES (?, ?, ?, ?)';
+		var sql = 'INSERT INTO points (hash, country, license, score) VALUES (?, ?, ?, ?)';
 		var values = [req.body.hash.toLowerCase(), req.body.country.toUpperCase(), req.body.license.toUpperCase(), upDown ? 1 : -1];
 		query(res, sql, values, send);
 	};
@@ -35,13 +35,13 @@ module.exports = function (router, db) {
 	});
 	
 	router.get('/score/:country/:license', function (req, res) {
-		var sql = `SELECT SUM(score) AS score FROM points WHERE country_to = ? AND license_to = ?`;
+		var sql = `SELECT SUM(score) AS score FROM points WHERE country = ? AND license = ?`;
 		var country = req.params.country.toUpperCase();
 		var license = req.params.license.toUpperCase();
 		var values = [country, license];
 		query(res, sql, values, rows => {
 			var score = rows[0].score;
-			sql = 'SELECT country_to AS country, license_to AS license, SUM(score) AS score FROM points GROUP BY country_to, license_to ORDER BY score DESC';
+			sql = 'SELECT country, license, SUM(score) AS score FROM points GROUP BY country, license ORDER BY score DESC';
 			query(res, sql, values, rows => {
 				var rank = _.findIndex(rows, {country, license}) + 1;
 				send(res, {score, rank});
@@ -50,7 +50,19 @@ module.exports = function (router, db) {
 	});
 	
 	var topBottom = function (req, res, isTop) {
-		var sql = 'SELECT country_to, license_to, SUM(score) AS score FROM points GROUP BY country_to, license_to ORDER BY score ' + (isTop ? 'DESC' : 'ASC') + ' LIMIT 100';
+		// var sql = 'SELECT country, license, SUM(score) AS score FROM points GROUP BY country, license ORDER BY score ' + (isTop ? 'DESC' : 'ASC') + ' LIMIT 100';
+		var sql = `
+				SELECT points.country, points.license, SUM(score / count) AS weighted_score FROM points
+					LEFT JOIN (
+						SELECT hash, COUNT(*) AS count FROM points
+						WHERE ts >= DATE_SUB(NOW(), INTERVAL 1 DAY)
+						GROUP BY hash
+					) AS counts
+					ON counts.hash = points.hash
+					WHERE points.ts >= DATE_SUB(NOW(), INTERVAL 1 MONTH)
+					GROUP BY country, license
+					ORDER BY weighted_score ${isTop ? 'DESC' : 'ASC'} LIMIT 100
+		`;
 		query(res, sql, [], send);
 	};
 	
