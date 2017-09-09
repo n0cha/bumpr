14e46cb21d726adb9f7331de84668312f70207d5
Niels van Reijmersdal <niels@vanreijmersdal.nl>
2017-09-09 17:46:57 +0200

    Added Selenium tests

---
+++ client/test/bumpr.pageobject.js
@@ -0,0 +1,95 @@
+const webdriver = require('selenium-webdriver');
+
+const By = webdriver.By;
+const Until = webdriver.until;
+
+module.exports = function PopupPageObject(options) {
+  if (!options.browser) throw Error('options.browser is required');
+  const browser = options.browser;
+
+  return {
+    open: (page = 'main') => {
+      if (page === 'main') browser.get(`http://localhost:3000/`);
+      if (page === 'ranking') browser.findElement(By.id('rankingButton')).click();
+    },
+    close: (page) => {
+      if (page === 'ranking') browser.findElement(By.id('back')).click();
+    },    
+    save: () => {
+      browser.findElement(By.id('save')).click();
+    },
+    thumbsUp: () => {
+      browser.findElement(By.id('like')).click();      
+    },
+    thumbsDown: () => {
+      browser.findElement(By.id('dislike')).click();      
+    },
+    getScore: (callback) => {
+      waitForVisible(By.id('score'), (el) => {
+        el.getText().then((text) => {
+          callback(text);          
+        });
+      });
+    },
+    getRank: (callback) => {
+      waitForVisible(By.id('rank'), (el) => {
+        el.getText().then((text) => {
+          callback(text);          
+        });
+      });
+    },
+    getMyLicense: (callback) => {
+      waitForVisible(By.id('me'), (el) => {
+        el.findElements(By.tagName('span')).then((els) => {
+          els[1].getText().then((text) => {
+            callback(text);          
+          });
+        });
+      });
+    },
+    getMessage: (callback) => {
+      waitForVisible(By.id('message'), (el) => {
+        el.getText().then((text) => {
+          callback(text);          
+        });
+      });
+    },
+    search: (inputText, callback) => {
+      waitForVisible(By.id('searchButton'), (el) => {
+        sleep(500).then(() => { // wait until onclick handler is bound
+          el.click();          
+          sleep(500).then(() => { // wait until animation is finished
+            browser.findElement(By.css('#searchInput input')).sendKeys(inputText + webdriver.Key.ENTER).then(() => {
+              waitForVisible(By.css('tr.search'), (row) => {
+                row.findElements(By.tagName('td')).then((tds) => {
+                  tds[1].getText().then((text) => {
+                    callback(text);          
+                  });
+                })                
+              })
+            });
+          });
+        });
+      });
+    },
+    fillPlateNumber: (inputText) => {
+      waitForVisible(By.id('plateNumber'), () => {
+        const input = browser.findElement(By.css('#plateNumber input'));
+        input.sendKeys(inputText);
+      });
+    },
+  }
+
+  function waitForVisible(locator, callback, timeout = 30000) {
+    browser.wait(Until.elementLocated(locator), timeout).then((found) => {
+      browser.wait(Until.elementIsVisible(found), timeout).then((el) =>{
+        callback(el)
+      });
+    });    
+  };
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  };
+
+};
---
+++ client/test/selenium.test.js
@@ -0,0 +1,86 @@
+const assert = require('assert');
+const webdriver = require('selenium-webdriver');
+const test = require('selenium-webdriver/testing');
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+
+const browser = constructBrowser();
+const app = require('./bumpr.pageobject.js')({ browser });
+
+test.describe('Bumpr', function tests() {
+  this.timeout(120000);
+  const TEST_LICENSE = 'TEST123';
+  const MY_LICENSE = '12ab34';
+
+  test.it('New user sign up', () => {
+    app.open();
+    app.fillPlateNumber(MY_LICENSE);
+    app.save();
+  });
+
+  test.it('Shows my license in the header', () => {
+    app.getMyLicense((license) => {
+      assert.equal(license, MY_LICENSE.toLocaleUpperCase());  
+    });
+  });
+
+  test.it('Shows score in the header', () => {
+    app.getScore((score) => {
+      assert.equal(score, 0);  
+    });
+  });
+
+  test.it('Shows rank in the header', () => {
+    app.getRank((rank) => {
+      assert.equal(rank, 0);  
+    });
+  });
+
+  test.it('Can give thumbsup', (done) => {
+    app.fillPlateNumber(TEST_LICENSE);
+    app.thumbsUp();
+    app.getMessage((message) => {
+      assert.equal(message, 'Thanks for the feedback!');
+      done();
+    });
+  });
+
+  test.it('Can give thumbsdown', (done) => {
+    app.open();    
+    app.fillPlateNumber(TEST_LICENSE);
+    app.thumbsDown();
+    app.getMessage((message) => {
+      assert.equal(message, 'Thanks for the feedback!');
+      done();
+    });
+  });
+
+  test.it('Can find license in ranking', (done) => {
+    app.open();    
+    app.open('ranking');
+    app.search(TEST_LICENSE, (rank) => {
+      assert.equal((rank > 0) , true);
+      done();
+    });
+  });
+
+  test.it('Can go back to main screen', (done) => {
+    app.close('ranking');
+    app.thumbsDown();
+    app.getMessage((message) => {
+      assert.equal(message, 'Missing plate number');
+      done();
+    });
+  }); 
+
+  test.after(() => {
+    browser.quit();
+  });
+});
+
+function constructBrowser() {
+  chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+  const options = new chrome.Options();
+  const capabilities = options.toCapabilities(webdriver.Capabilities.chrome());
+  return new webdriver.Builder().withCapabilities(capabilities).build();
+}
